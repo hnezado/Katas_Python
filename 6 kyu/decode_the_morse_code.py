9dd3baaed61cf912cd9abd8e7a35986b6380e4da
Hector <Thornnk1@gmail.com>
2021-08-14 00:20:33 +0200

    Decode the Morse code

--- 6 kyu/decode_the_morse_code.py
+++ 6 kyu/decode_the_morse_code.py
@@ -1,11 +1,19 @@
-# In this kata you have to write a simple Morse code decoder. While the Morse code is now mostly superseded by voice and digital data communication channels, it still has its use in some applications around the world.
-# The Morse code encodes every character as a sequence of "dots" and "dashes". For example, the letter A is coded as ·−, letter Q is coded as −−·−, and digit 1 is coded as ·−−−−. The Morse code is case-insensitive, traditionally capital letters are used. When the message is written in Morse code, a single space is used to separate the character codes and 3 spaces are used to separate words. For example, the message HEY JUDE in Morse code is ···· · −·−−   ·−−− ··− −·· ·.
+# In this kata you have to write a simple Morse code decoder. While the Morse code is now mostly superseded by voice
+# and digital data communication channels, it still has its use in some applications around the world.
+# The Morse code encodes every character as a sequence of "dots" and "dashes". For example, the letter A is coded as ·−,
+# letter Q is coded as −−·−, and digit 1 is coded as ·−−−−. The Morse code is case-insensitive, traditionally capital
+# letters are used. When the message is written in Morse code, a single space is used to separate the character codes
+# and 3 spaces are used to separate words. For example,
+# the message HEY JUDE in Morse code is ···· · −·−−   ·−−− ··− −·· ·.
 #
 # NOTE: Extra spaces before or after the code have no meaning and should be ignored.
 #
-# In addition to letters, digits and some punctuation, there are some special service codes, the most notorious of those is the international distress signal SOS (that was first issued by Titanic), that is coded as ···−−−···. These special codes are treated as single special characters, and usually are transmitted as separate words.
+# In addition to letters, digits and some punctuation, there are some special service codes, the most notorious of
+# those is the international distress signal SOS (that was first issued by Titanic), that is coded as ···−−−···.
+# These special codes are treated as single special characters, and usually are transmitted as separate words.
 #
-# Your task is to implement a function that would take the morse code as input and return a decoded human-readable string.
+# Your task is to implement a function that would take the morse code as input and return a decoded
+# human-readable string.
 #
 # For example:
 #
@@ -18,7 +26,8 @@
 # Coffeescript/C++/Go/JavaScript/Julia/PHP/Python/Ruby/TypeScript: MORSE_CODE['.--']
 # C#: MorseCode.Get(".--") (returns string)
 # F#: MorseCode.get ".--" (returns string)
-# Elixir: @morse_codes variable (from use MorseCode.Constants). Ignore the unused variable warning for morse_codes because it's no longer used and kept only for old solutions.
+# Elixir: @morse_codes variable (from use MorseCode.Constants). Ignore the unused variable warning for morse_codes
+# because it's no longer used and kept only for old solutions.
 # Elm: MorseCodes.get : Dict String String
 # Haskell: morseCodes ! ".--" (Codes are in a Map String String)
 # Java: MorseCode.get(".--")
@@ -29,16 +38,59 @@
 # Swift: MorseCode[".--"] ?? "" or MorseCode[".--", default: ""]
 # C: provides parallel arrays, i.e. morse[2] == "-.-" for ascii[2] == "C"
 # NASM: a table of pointers to the morsecodes, and a corresponding list of ascii symbols
-# All the test strings would contain valid Morse code, so you may skip checking for errors and exceptions. In C#, tests will fail if the solution code throws an exception, please keep that in mind. This is mostly because otherwise the engine would simply ignore the tests, resulting in a "valid" solution.
+# All the test strings would contain valid Morse code, so you may skip checking for errors and exceptions. In C#,
+# tests will fail if the solution code throws an exception, please keep that in mind. This is mostly because otherwise
+# the engine would simply ignore the tests, resulting in a "valid" solution.
 #
 # Good luck!
-#
-# After you complete this kata, you may try yourself at Decode the Morse code, advanced.
+
+MORSE_CODE = {
+  '.-': 'A',
+  '-...': 'B',
+  '-.-.': 'C',
+  '-..': 'D',
+  '.': 'E',
+  '..-.': 'F',
+  '--.': 'G',
+  '....': 'H',
+  '..': 'I',
+  '.---': 'J',
+  '-.-': 'K',
+  '.-..': 'L',
+  '--': 'M',
+  '-.': 'N',
+  '---': 'O',
+  '.--.': 'P',
+  '--.-': 'Q',
+  '.-.': 'R',
+  '...': 'S',
+  '-': 'T',
+  '..-': 'U',
+  '...-': 'V',
+  '.--': 'W',
+  '-..-': 'X',
+  '-.--': 'Y',
+  '--..': 'Z',
+  '-----': '0',
+  '.----': '1',
+  '..---': '2',
+  '...--': '3',
+  '....-': '4',
+  '.....': '5',
+  '-....': '6',
+  '--...': '7',
+  '---..': '8',
+  '----.': '9',
+}
 
 
 def decodeMorse(morse_code):
-    # ToDo: Accept dots, dashes and spaces, return human-readable message
-    return morse_code.replace('.', MORSE_CODE['.']).replace('-', MORSE_CODE['-']).replace(' ', '')
+    splitted_msg = morse_code.split('   ')
+    splitted_msg = [msg.split(' ') for msg in splitted_msg]
+    print('adsf', splitted_msg)
+    return ''.join(MORSE_CODE[char] for char in splitted_msg)
+    # # ToDo: Accept dots, dashes and spaces, return human-readable message
+    # return morse_code.replace('.', MORSE_CODE['.']).replace('-', MORSE_CODE['-']).replace(' ', '')
 
 
 print(decodeMorse('.... . -.--   .--- ..- -.. .'))
