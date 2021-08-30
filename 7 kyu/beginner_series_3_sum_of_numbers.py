ae65cbd2e3e1f6cb29b5633c6b2cfc9e0deca8b7
Thornnk <Thornnk1@gmail.com>
2021-08-30 21:46:31 +0200

    Beginner Series #3 Sum of Numbers

---
+++ 7 kyu/beginner_series_3_sum_of_numbers.py
@@ -0,0 +1,21 @@
+# Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including
+# them and return it. If the two numbers are equal return a or b.
+#
+# Note: a and b are not ordered!
+# Examples
+#
+# get_sum(1, 0) == 1   // 1 + 0 = 1
+# get_sum(1, 2) == 3   // 1 + 2 = 3
+# get_sum(0, 1) == 1   // 0 + 1 = 1
+# get_sum(1, 1) == 1   // 1 Since both are same
+# get_sum(-1, 0) == -1 // -1 + 0 = -1
+# get_sum(-1, 2) == 2  // -1 + 0 + 1 + 2 = 2
+
+import functools as ft
+
+
+def get_sum(a, b):
+	return ft.reduce(lambda x, y: x+y, [num if a != b else a for num in range(min(a, b), max(a, b)+1)])
+
+
+print(get_sum(2, -3))
