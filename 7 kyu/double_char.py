12565983cf3c963b2b3d12654ec4f447694496c5
Hector <Thornnk1@gmail.com>
2021-10-20 22:40:35 +0200

    Double Char

---
+++ 7 kyu/double_char.py
@@ -0,0 +1,14 @@
+# Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+#
+# double_char("String") ==> "SSttrriinngg"
+#
+# double_char("Hello World") ==> "HHeelllloo  WWoorrlldd"
+#
+# double_char("1234!_ ") ==> "11223344!!__  "
+# Good Luck!
+
+def double_char(s):
+    return ''.join([c*2 for c in s])
+
+
+print(double_char('Hello World'))
